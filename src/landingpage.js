1bf5275a86cdebbd3d5cc251f78b3dce23708670
GurkiratSidhu <g3sidhu@gmail.com>
2021-06-20 18:00:59 +0530

    added mediaqueries using breakpoints

--- src/landingpage.js
+++ src/landingpage.js
@@ -5,7 +5,7 @@ function Landingpage() {
     return (
        
             <header class="header">
-                <nav class="flex flex-jc-sb">
+                <nav class="flex flex-jc-sb flex-ai-c">
                     <a href="/" className="header__logo">
                         <img src={logo} alt="Easybank"/>
                     </a>
@@ -15,7 +15,17 @@ function Landingpage() {
                         <span></span>
                         <span></span>
 
+               
                     </a>
+                   <div class="header__links">
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact</a>
+                    <a href="#">Blog</a>
+                    <a href="#">Careers</a>
+                    </div> 
+
+                    <a href="#" className="button header__cta">Request Invite</a>
                 </nav>
                
 
