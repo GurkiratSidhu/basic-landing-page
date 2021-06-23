33cc6a5c4717f9725e99b0464295ed9d18c9341b
GurkiratSidhu <g3sidhu@gmail.com>
2021-06-23 19:11:39 +0530

    commit

--- src/landingpage.js
+++ src/landingpage.js
@@ -3,7 +3,7 @@ import logo from './images/logo.svg'
 
 function Landingpage() {
     return (
-       
+       <body>
             <header class="header">
                 <nav class="flex flex-jc-sb flex-ai-c">
                     <a href="/" className="header__logo">
@@ -27,13 +27,24 @@ function Landingpage() {
 
                     <a href="/#" className="button header__cta hide-for-mobile">Request Invite</a>
                 </nav>
-               
-
-
-
             </header>
+        
 
+            <section className="hero">
+                <div className="hero__image">
+                </div>
 
+                <div className="hero__text">
+                     <h1>
+                        Next geenration digital banking
+                    </h1>
+
+                    <p>Take your financial life online, Your EasyBank account will be for spending, saving, budgeting, investing, and so much more.</p>
+                    <a href="/#" className="button hero__cta">Request Invite</a>
+                </div>
+            </section>
+        </body>
+        
     
     )
 }
