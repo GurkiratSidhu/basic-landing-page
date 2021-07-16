349621efe0be5cae71d99361b7dd598db3c06cd8
GurkiratSidhu <g3sidhu@gmail.com>
2021-07-16 18:28:37 +0530

    commit

--- src/landingpage.js
+++ src/landingpage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import logo from "./images/logo.svg";
 import image1 from "./images/icon-online.svg";
+import image2 from "./images/icon-budgeting.svg";
+import image3 from "./images/icon-onboarding.svg";
+import image4 from "./images/icon-api.svg";
 
 function Landingpage() {
 	return (
@@ -55,8 +58,8 @@ function Landingpage() {
 			<section className="feature container continer--pall">
 				<h2>Why choose Easybank?</h2>
 				<p>
-					We leverage Open Banking to turn your bank account into your finances
-					like never before.
+				We leverage Open Banking to turn your bank account into your financial hub. Control 
+              your finances like never before.
 				</p>
 				<div className="feature__grid">
 					<div className="feature__item">
@@ -65,41 +68,38 @@ function Landingpage() {
 						</div>
 						<div className="feature__title">Online Banking</div>
 						<div className="feature__description">
-							Our modern mobile and web applications wherever you are in the
-							world.
+						Our modern web and mobile applications allow you to keep track of your finances 
+              wherever you are in the world.
 						</div>
 					</div>
 
 					<div className="feature__item">
 						<div className="feature__icon">
-							<img src={image1} alt="Image1" />
+							<img src={image2} alt="Image2" />
 						</div>
-						<div className="feature__title">Online Banking</div>
+						<div className="feature__title">Simple Budgeting</div>
 						<div className="feature__description">
-							Our modern mobile and web applications wherever you are in the
-							world.
+						See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.
 						</div>
 					</div>
 
 					<div className="feature__item">
 						<div className="feature__icon">
-							<img src={image1} alt="Image1" />
+							<img src={image3} alt="Image3" />
 						</div>
-						<div className="feature__title">Online Banking</div>
+						<div className="feature__title">Fast Onboarding</div>
 						<div className="feature__description">
-							Our modern mobile and web applications wherever you are in the
-							world.
+						We don’t do branches. Open your account in minutes online and start taking control of your finances right away.
 						</div>
 					</div>
 
 					<div className="feature__item">
 						<div className="feature__icon">
-							<img src={image1} alt="Image1" />
+							<img src={image4} alt="Image4" />
 						</div>
-						<div className="feature__title">Online Banking</div>
+						<div className="feature__title">Open API</div>
 						<div className="feature__description">
-							Our modern mobile and web applications wherever you are in the
-							world.
+						Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.
 						</div>
 					</div>
 				</div>
