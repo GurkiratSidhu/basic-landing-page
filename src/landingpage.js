e93951dea3873d8e95f4ca754ab57048c232153d
GurkiratSidhu <g3sidhu@gmail.com>
2021-07-17 18:07:48 +0530

    commit

--- src/landingpage.js
+++ src/landingpage.js
@@ -56,11 +56,14 @@ function Landingpage() {
 			</section>
 
 			<section className="feature container continer--pall">
-				<h2>Why choose Easybank?</h2>
-				<p>
-				We leverage Open Banking to turn your bank account into your financial hub. Control 
-              your finances like never before.
-				</p>
+				<div className="feature__intro">
+					<h2>Why choose Easybank?</h2>
+					<p>
+						We leverage Open Banking to turn your bank account into your
+						financial hub. Control your finances like never before.
+					</p>
+				</div>
+
 				<div className="feature__grid">
 					<div className="feature__item">
 						<div className="feature__icon">
@@ -68,8 +71,8 @@ function Landingpage() {
 						</div>
 						<div className="feature__title">Online Banking</div>
 						<div className="feature__description">
-						Our modern web and mobile applications allow you to keep track of your finances 
-              wherever you are in the world.
+							Our modern web and mobile applications allow you to keep track of
+							your finances wherever you are in the world.
 						</div>
 					</div>
 
@@ -79,7 +82,8 @@ function Landingpage() {
 						</div>
 						<div className="feature__title">Simple Budgeting</div>
 						<div className="feature__description">
-						See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.
+							See exactly where your money goes each month. Receive
+							notifications when you’re close to hitting your limits.
 						</div>
 					</div>
 
@@ -89,7 +93,8 @@ function Landingpage() {
 						</div>
 						<div className="feature__title">Fast Onboarding</div>
 						<div className="feature__description">
-						We don’t do branches. Open your account in minutes online and start taking control of your finances right away.
+							We don’t do branches. Open your account in minutes online and
+							start taking control of your finances right away.
 						</div>
 					</div>
 
@@ -99,7 +104,8 @@ function Landingpage() {
 						</div>
 						<div className="feature__title">Open API</div>
 						<div className="feature__description">
-						Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.
+							Manage your savings, investments, pension, and much more from one
+							account. Tracking your money has never been easier.
 						</div>
 					</div>
 				</div>
